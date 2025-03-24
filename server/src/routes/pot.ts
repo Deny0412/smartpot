522fdacc05069cf29eb210bfd9185e03b75384dc
MatejSoukup <90925007+MatejSoukup@users.noreply.github.com>
2025-03-24 22:59:22 +0100

    auth middleware

--- server/src/routes/pot.ts
+++ server/src/routes/pot.ts
@@ -1,8 +1,9 @@
 import { FastifyInstance } from 'fastify';
 import { potController } from '../controller/pot-controller';
+import { authMiddleware } from '../middleware/auth-middleware';
 
 export default async function potRoutes(fastify: FastifyInstance) {
-    fastify.post('/add', potController.create);
+    fastify.post('/add', { preHandler: authMiddleware }, potController.create);
     fastify.delete('/delete/:id', potController.delete);
     fastify.get('/get/:id', potController.get);
     fastify.get('/list/:householdId', potController.list);
