0b886a252cf5e2a03a09090d3c61e0a11ae9eca9
Mikuláš Novák <miki.novak5@gmail.com>
2025-03-24 23:10:34 +0100

    household ABL fix + household controller/routes

--- server/src/abl/household/household-abl.ts
+++ server/src/abl/household/household-abl.ts
@@ -1,84 +1,29 @@
-import potDao from "../../dao/pot/pot-dao"; // Adjust the import based on your DAO structure
-import { IPot } from "@/models/Pot";
-import createPotHandler from "./pot-create-abl";
-import listPotsHandler from "./pot-list-abl";
-import getPotHandler from "./pot-get-abl";
+import { IHousehold } from "../../models/Household";
+import createHouseholdAbl from "./household-create-abl";
+import updateHouseholdAbl from "./household-update-abl";
+import getHouseholdAbl from "./household-get-abl";
+import deleteHouseholdAbl from "./household-delete-abl";
+
 import { FastifyRequest, FastifyReply } from "fastify";
-import Ajv from "ajv";
-import { sendClientError } from "../../middleware/response-handler";
 
-// TODO: validace pomocí ajv
-const ajv = new Ajv();
-
-const SCHEMA_POT = {
-  type: "object",
-  properties: {
-    id_profile: { type: "string" },
-    name: { type: "string" },
-  },
-};
-
-class PotAbl {
-  static async create(data: IPot, reply: FastifyReply) {
-    try {
-      const valid = ajv.validate(SCHEMA_POT, data);
-      if (!valid) {
-        sendClientError(reply, "Invalid pot data", 400);
-      }
-      return await createPotHandler(data, reply); // Call the handler to create the pot
-    } catch (error: unknown) {
-      throw error;
-    }
-  }
-
-  static async update(id: string, data: IPot) {
-    try {
-      const updatedPot = await potDao.updatePot(id, data);
-      if (!updatedPot) {
-        throw new Error("Pot not found");
-      }
-      return updatedPot; // Return the updated pot
-    } catch (error: unknown) {
-      if (error instanceof Error) {
-        throw new Error("Failed to update pot: " + error.message);
-      } else {
-        throw new Error("Failed to update pot: Unknown error occurred");
-      }
-    }
-  }
-
-  static async delete(id: string) {
-    try {
-      await potDao.deletePot(id);
-      return null; // Return null to indicate successful deletion
-    } catch (error: unknown) {
-      if (error instanceof Error) {
-        throw new Error("Failed to delete pot: " + error.message);
-      } else {
-        throw new Error("Failed to delete pot: Unknown error occurred");
-      }
-    }
-  }
-
-  static async list(request: FastifyRequest, reply: FastifyReply) {
-    try {
-      return await listPotsHandler(request, reply);
-    } catch (error: unknown) {
-      if (error instanceof Error) {
-        throw new Error("Failed to list pots: " + error.message);
-      } else {
-        throw new Error("Failed to list pots: Unknown error occurred");
-      }
-    }
-  }
-
-  static async get(id: string, reply: FastifyReply) {
-    try {
-      return await getPotHandler(id, reply);
-    } catch (error: unknown) {
-      throw error;
-    }
-  }
+async function createHousehold(data: IHousehold, reply: FastifyReply) {
+  return await createHouseholdAbl(data, reply);
 }
 
-export default PotAbl;
+async function updateHousehold(data: IHousehold, reply: FastifyReply) {
+  return await updateHouseholdAbl(data, reply);
+}
+
+async function deleteHousehold(id: string, reply: FastifyReply) {
+  return await deleteHouseholdAbl(id, reply);
+}
+async function getHousehold(id: string, reply: FastifyReply) {
+  return await getHouseholdAbl(id, reply);
+}
+
+export default {
+  createHousehold,
+  updateHousehold,
+  deleteHousehold,
+  getHousehold,
+};
--- server/src/controller/household-controller.ts
+++ server/src/controller/household-controller.ts
@@ -1,43 +1,55 @@
-import { FastifyInstance, FastifyRequest, FastifyReply } from "fastify";
-import CREATE from "../abl/household/household-create-abl";
-import DELETE from "../abl/household/household-delete-abl";
-import GET from "../abl/household/household-get-abl";
-import UPDATE from "../abl/household/household-update-abl";
-import LIST from "../abl/household/household-list-abl";
+import { FastifyRequest, FastifyReply } from "fastify";
+import householdAbl from "../abl/household/household-abl";
+import {
+  sendSuccess,
+  sendCreated,
+  sendError,
+  sendInternalServerError,
+} from "../middleware/response-handler";
+import { IHousehold } from "../models/Household";
 
-export default async function householdRoutes(fastify: FastifyInstance) {
-  fastify.post(
-    "/household/create",
-    async (request: FastifyRequest, reply: FastifyReply) => {
-      return CREATE(request, reply);
-    }
-  );
-
-  fastify.delete(
-    "/household/delete",
-    async (request: FastifyRequest, reply: FastifyReply) => {
-      return DELETE(request, reply);
-    }
-  );
-
-  fastify.get(
-    "/household/get",
-    async (request: FastifyRequest, reply: FastifyReply) => {
-      return GET(request, reply);
-    }
-  );
-
-  fastify.put(
-    "/household/update",
-    async (request: FastifyRequest, reply: FastifyReply) => {
-      return UPDATE(request, reply);
-    }
-  );
-
-  fastify.get(
-    "/household/list",
-    async (request: FastifyRequest, reply: FastifyReply) => {
-      return LIST(request, reply);
-    }
-  );
+interface Params {
+  id: string;
 }
+
+export const householdController = {
+  create: async (request: FastifyRequest, reply: FastifyReply) => {
+    try {
+      const reqParam = request.body as IHousehold;
+      const newHousehold = await householdAbl.createHousehold(reqParam, reply);
+      sendCreated(reply, newHousehold, "Household created successfully");
+    } catch (error) {
+      console.log(error);
+      sendInternalServerError(reply);
+    }
+  },
+  delete: async (request: FastifyRequest, reply: FastifyReply) => {
+    try {
+      const id = (request.params as Params).id;
+      await householdAbl.deleteHousehold(id, reply);
+    } catch (error) {
+      sendError(reply, error);
+    }
+  },
+  get: async (request: FastifyRequest, reply: FastifyReply) => {
+    try {
+      const id = (request.params as Params).id;
+      const response = await householdAbl.getHousehold(id, reply);
+      sendSuccess(reply, response, "Household retrieved successfully");
+    } catch (error) {
+      sendError(reply, error);
+    }
+  },
+  update: async (request: FastifyRequest, reply: FastifyReply) => {
+    try {
+      const updatedHousehold = request.body as IHousehold;
+      const response = await householdAbl.updateHousehold(
+        updatedHousehold,
+        reply
+      );
+      sendSuccess(reply, response, "Household updated successfully");
+    } catch (error) {
+      sendError(reply, error);
+    }
+  },
+};
