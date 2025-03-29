52b9404381aaf5989b9a4fdddd5544d72ceb093c
Mikuláš Novák <miki.novak5@gmail.com>
2025-03-29 16:15:19 +0100

    Household cleanup V2

--- server/src/dao/household/household-get-dao.ts
+++ server/src/dao/household/household-get-dao.ts
@@ -1,6 +1,6 @@
 import HOUSEHOLD_MODEL from "../../models/Household";
-async function getHousehold(id: string) {
+async function getHouseholdDao(id: string) {
   return await HOUSEHOLD_MODEL.findById(id);
 }
 
-export default getHousehold;
+export default getHouseholdDao;
