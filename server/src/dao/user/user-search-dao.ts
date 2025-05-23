d359fad9638012286245c1f544c2befa5a2b1357
Mikuláš Novák <miki.novak5@gmail.com>
2025-05-23 11:30:31 +0200

    user Search fix

--- server/src/dao/user/user-search-dao.ts
+++ server/src/dao/user/user-search-dao.ts
@@ -2,9 +2,14 @@ import USER_MODEL from "../../models/User";
 
 async function userSearchDao(query: string) {
   return await USER_MODEL.find({
-    email: { $regex: query, $options: "i" }, // case-insensitive match
+    $or: [
+      { email: { $regex: query, $options: "i" } },
+      { name: { $regex: query, $options: "i" } },
+      { surname: { $regex: query, $options: "i" } },
+    ],
   })
     .select("email name surname")
     .lean();
 }
+
 export default userSearchDao;
